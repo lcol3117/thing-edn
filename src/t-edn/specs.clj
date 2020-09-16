9b60169879d220fff307fb2c8576db3e4d96a7a5
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-09-16 14:24:06 -0400

    work on it

--- src/t-edn/specs.clj
+++ src/t-edn/specs.clj
@@ -1,2 +1,23 @@
 (ns t-edn.specs
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.alpha.spec :as s]))
+
+(s/def ::id (s/and int? #(>= % 0)))
+(s/def ::class keyword?)
+(s/def ::loc keyword?)
+
+(s/def ::id-thing-schema
+  (s/schema [::id]))
+(s/def ::loc-thing-schema
+  (s/schema [::class ::loc]))
+(s/def ::thing-schema
+  (s/union id-thing-schema loc-thing-schema))
+
+(s/def ::id-thing-spec)
+(s/def ::loc-thing-spe)
+(s/def ::thing-spec)
+
+(s/def ::target keyword?)
+(s/def ::task keyword?)
+
+(s/def ::intent-schema
+  (s/schema [::target ::task]))
